e1ba28393de7c62ecf617307f6b3bad136ad046c
msk1 <skarbala.martin@gmail.com>
2017-04-27 18:10:39 +0200

    check if vacations are empty

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,12 @@
         <h3>Najnovsie ziadosti</h3>
         <div class="row">
             <div class="col-md-6">
-            @include('partials.vacation_table', ['vacations' => $vacations])
+            @if ($vacations->isEmpty())
+                <h4>zatial nemas ziadne dovolenky</h4>
+            @else
+                 @include('partials.vacation_table', ['vacations' => $vacations])
+            @endif
+
             </div>
             <div class="col-md-6" style="text-align: center">
                 <p>mozes si este naplanovat</p>
