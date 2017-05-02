59215f94edbcad49f4f47b4ad50eba12cfc13233
martin.skarbala <martin.skarbala@itera.no>
2017-05-02 16:42:16 +0200

    updated table view

--- resources/views/all_vacations.blade.php
+++ resources/views/all_vacations.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>vsetky vakacie</h1>
-	@include('partials.vacation_table', ['vacations' => $vacations])
+    <div class="col-md-8 col-md-offset-2">
+        <h1>vsetky vakacie</h1>
+        @include('partials.vacation_table', ['vacations' => $vacations])
+    </div>
 @stop
--- resources/views/partials/vacation_table.blade.php
+++ resources/views/partials/vacation_table.blade.php
@@ -1,9 +1,10 @@
-<table class="table table-bordered">
+<table class="table">
     <thead>
         <th>Od</th>
         <th>do</th>
         <th>pocet dni</th>
         <th>stav</th>
+        <th></th>
     </thead>
     <tbody>
         @foreach($vacations as $vacation)
@@ -12,6 +13,7 @@
             <td>{{$vacation->vacation_to}}</td>
             <td>{{$vacation->days_of_vacation}}</td>
             <td>{{$vacation->status->description}}</td>
+            <td class="row"><a href="">Detail</a></td>
         </tr>
         @endforeach
     </tbody>
