b610d141a1e0fe26a38895d5929fd41adf0cd588
msk1 <skarbala.martin@gmail.com>
2017-05-03 20:46:41 +0200

    updated vacation detail blade

--- resources/views/vacation_detail.blade.php
+++ resources/views/vacation_detail.blade.php
@@ -1,33 +1,32 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="col-md-8 col-md-offset-4">
-        <form action="" class="form-horizontal">
-        <h4>Detail dovolenky</h4>
+@extends('layouts.app') @section('content')
+<h4 class="text-center">Detail dovolenky</h4>
+<div class="col-md-8 col-md-offset-2">
+    <form action="" class="form-horizontal">
         <hr>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Dovolenka od</label>
-                <input type="email" class="form-control disabled" id="disabledInput"
-                value=" {{$vacation->vacation_from}}" disabled>
+        <div class="form-group">
+            <label for="vacation_from" class="col-md-4 control-label">Začiatok dovolenky</label>
+            <div class="col-md-6">
+                <input id="vacation_from" type="date" class="form-control" name="vacation_from" value="{{ $vacation->vacation_from}}" disabled>
             </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">Dovolenka do</label>
-                <p class="form-control-static">
-                    {{$vacation->vacation_to}}
-                </p>
+        </div>
+        <div class="form-group">
+            <label for="vacation_to" class="col-md-4 control-label">Koniec dovolenky</label>
+            <div class="col-md-6">
+                <input id="vacation_to" type="date" class="form-control" name="vacation_to" value="{{ $vacation->vacation_to}}" disabled>
             </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">pocet dni</label>
-                <p class="form-control-static">
-                    {{$vacation->days_of_vacation}}
-                </p>
+        </div>
+        <div class="form-group">
+            <label class="col-md-4 control-label">Počet dní</label>
+            <div class="col-md-6">
+                <p class="form-control-static">{{$vacation->days_of_vacation}}</p>
             </div>
-            <div class="form-group">
-                <label class="col-sm-2 control-label">stav</label>
-                <p class="form-control-static">
-                {{$vacation->status->description}}
+        </div>
+        <div class="form-group">
+            <label class="col-md-4 control-label">Stav</label>
+            <div class="col-md-6">
+                <p class="form-control-static">{{$vacation->status->description}}</p>
             </div>
-            </p>
-        </form>
-    </div>
+        </div>
+    </form>
+</div>
 @stop
