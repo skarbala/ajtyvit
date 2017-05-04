ba58c7c10fbbd95456cbbc99302b23eab9dfdbe0
martin.skarbala <martin.skarbala@itera.no>
2017-05-04 09:31:03 +0200

    extracted nav to separate template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,20 @@
 @extends('layouts.app') @section('content')
-<div class="container">
+    <div class="container">
 
-<div class="row" style="margin-top: 30px">
-    <div class="col-md-5 col-md-offset-1">
-        @if ($vacations->isEmpty())
-            <h4 style="margin-top: 0px">Zatiaľ nemáš žiadnu žiadosť</h4>
-        @else
-            <h4 style="margin-bottom: 20px;margin-top: 0px">Najnovšie žiadosti o dovolenku</h4>
-            @include('partials.vacation_table', ['vacations' => $vacations])
-        @endif
+        <div class="row" style="margin-top: 30px">
+            <div class="col-md-5 col-md-offset-1">
+                @if ($vacations->isEmpty())
+                    <h4 style="margin-top: 0px">Zatiaľ nemáš žiadnu žiadosť</h4>
+                @else
+                    <h4 style="margin-bottom: 20px;margin-top: 0px">Najnovšie žiadosti o dovolenku</h4>
+                    @include('partials.vacation_table', ['vacations' => $vacations])
+                @endif
+            </div>
+            <div class="col-md-5" style="text-align: center">
+                <p>môžeš si ešte naplánovať</p>
+                <p style="font-size: 60px">{{Auth::user()->getDaysOfVacationLeft()}}</p>
+                <p>dní</p>
+            </div>
+        </div>
     </div>
-    <div class="col-md-5" style="text-align: center">
-        <p>môžeš si ešte naplánovať</p>
-        <p style="font-size: 60px">{{Auth::user()->getDaysOfVacationLeft()}}</p>
-        <p>dní</p>
-    </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
-    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/ico" />
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/ico"/>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -26,27 +26,27 @@
             color: #be1968;
         }
 
-         .navbar-default a.navbar-brand:hover {
+        .navbar-default a.navbar-brand:hover {
             color: #002060;
         }
 
-
-        .navbar-default{
+        .navbar-default {
             background-color: #fff;
 
         }
-        .navbar-default .navbar-nav>li>a{
+
+        .navbar-default .navbar-nav > li > a {
             color: #cd2c77;
             font-weight: 600;
             text-transform: uppercase;
         }
 
-        .navbar-default .navbar-nav>li>a:hover, tbody a:hover{
+        .navbar-default .navbar-nav > li > a:hover, tbody a:hover {
             text-decoration: none;
             color: #002060;
         }
 
-        tbody a{
+        tbody a {
             color: #cd2c77;
             font-weight: 600;
         }
@@ -55,66 +55,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
-
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}" style="font-family:'Shadows Into Light Two'">
-                    Dovolenkovač
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{url('new_vacation')}}">Nová žiadosť</a></li>
-                    <li><a href="{{url('allvacations')}}">moje žiadosti</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                               aria-expanded="false">
-                                {{ Auth::user()->name}}
-                                @if(Auth::user()->isAdmin())
-                                    <span>admin</span>
-                                @endif
-                            </a>
-
-                        {{--<ul class="dropdown-menu" role="menu">--}}
-                        <li>
-                            <a href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                odhlásiť sa
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+    @include('partials.navigation')
     @yield('content')
 </div>
 
