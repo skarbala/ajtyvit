68f1e1bcf23adffcd9fb8a5f15bf6069187354b2
martin.skarbala <martin.skarbala@itera.no>
2017-05-09 15:31:25 +0200

    updated store method

--- app/Http/Controllers/VacationController.php
+++ app/Http/Controllers/VacationController.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
+use Nuxia\BusinessDayManipulator\DatePeriod;
 use Nuxia\BusinessDayManipulator\LocalizedManipulator;
 use Nuxia\BusinessDayManipulator\Manipulator;
 
@@ -43,9 +44,11 @@ class VacationController extends Controller
 
         $vacationDays = $this->countDaysOfVacation($request->vacation_from, $request->vacation_to);
 
-
+        if ($vacationDays == 0){
+            return redirect()->back()->withErrors(['msg' => 'netreba ti dovolenku']);
+        }
         if ($vacationDays > Auth::user()->getDaysOfVacationLeft()) {
-            return redirect('/new_vacation')->withErrors(['msg' => 'mas malo dovolenky']);
+            return redirect()->back()->withErrors(['msg' => 'mas malo dovolenky']);
         }
         Vacation::create([
             'vacation_from' => $request['vacation_from'],
@@ -124,7 +127,34 @@ class VacationController extends Controller
      */
     public function countDaysOfVacation($vacation_from, $vacation_to)
     {
-        $manipulator = new Manipulator();
+        $holidays = [
+            new DatePeriod(new \DateTime('2017-12-24'), new \DateTime('2017-12-26')),
+            new DatePeriod(new \DateTime('2018-12-24'), new \DateTime('2018-12-26')),
+        ];
+
+        $freeDays = [
+            new \DateTime('2017-01-01'),
+            new \DateTime('2017-01-06'),
+            new \DateTime('2017-04-14'),
+            new \DateTime('2017-04-17'),
+            new \DateTime('2017-05-01'),
+            new \DateTime('2017-05-08'),
+            new \DateTime('2017-07-05'),
+            new \DateTime('2017-08-29'),
+            new \DateTime('2017-09-01'),
+            new \DateTime('2017-09-15'),
+            new \DateTime('2017-11-01'),
+            new \DateTime('2017-11-17'),
+        ];
+
+        $freeWeekDays = [
+            Manipulator::SATURDAY,
+            Manipulator::SUNDAY
+        ];
+
+        $manipulator = new Manipulator($freeDays, $freeWeekDays, $holidays);
+
+
         $manipulator->setStartDate(new \DateTime($vacation_from));
         $manipulator->setEndDate(new \DateTime($vacation_to));
 
